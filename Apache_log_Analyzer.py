af222de9cc53255ca545d873ebd87f4236b75732
Abdullah <aaaltameemi@madisoncollege.edu>
2023-07-01 10:36:06 -0500

    Apache_log_Analyzer.py

---
+++ Apache_log_Analyzer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+#Author name :Abdullah Altameemi
+#Project Description: Creating Apache in the entrprice envirnoment hosting web to process the  request that recieve by web server .
+#Author email:aaaltameemi@madisoncollege.com  
+
+'''
+#create welcome message about the script and print it to the screen .
+welcome_message ="welcome to Apache analyzer!"
+print(welcome_message)
+
