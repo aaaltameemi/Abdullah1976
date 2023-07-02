05be64823b97a92a63f9c5d093e22ce3322a090a
Abdullah <aaaltameemi@madisoncollege.edu>
2023-07-02 14:43:20 -0500

    Apache_log_Analyzer

--- Apache_log_Analyzer.py
+++ Apache_log_Analyzer.py
@@ -12,15 +12,20 @@ print(welcome_message)
 user_message=input("would like to continue?please enter your 'y' for yes amd 'n for No: ")
 print(user_message)
 #creat string variable
-apache_log='111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"'
+apache_log='''111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"
+111.222.333.124 HOME - [01/Feb/1998:01:08:46 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 28083 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"
+111.222.333.125 AWAY - [01/Feb/1998:01:08:53 -0800] "GET /bannerad/ad7.gif HTTP/1.0" 401 9332 "http://www.referrer.com/bannerad/ba_ad.htm" "Mozilla/4.01 (Macintosh; I; PPC)"
+111.222.333.126 AWAY - [01/Feb/1998:01:09:14 -0800] "GET /bannerad/click.htm HTTP/1.0" 501 207 "http://www.referrer.com/bannerad/menu.htm" "Mozilla/4.01 (Macintosh; I; PPC)"'''
 
-IP_address=apache_log[0:15]
-
-print(f"log format request:***{IP_address}***")
-#spilt filed s of the  apache log entry .
-fields=apache_log.split(' ')
-#create fields type.
-fields_type=type(fields)
-#create return ode
-return_cod=fields[8]
-print(f"return code: {return_cod}")
+apache_log_list=apache_log.split('\n')
+#loop through apache_log_list
+for apache_log in apache_log_list:
+    fields=apache_log.split(' ')
+    IP_address=fields[0]
+    return_code1=200
+    return_code2=401
+    return_code3=501
+print(f"log request from:{IP_address} \nReturn code:{return_code1}")
+print(f"log request from:{IP_address} \nReturn code:{return_code1}") 
+print(f"log request from:{IP_address} \nReturn code:{return_code2}")
+print(f"log request from:{IP_address} \nReturn code:{return_code3}")
