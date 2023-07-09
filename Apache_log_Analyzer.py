434bc5f35c046990a99e2108dfbca81626ee320b
Abdullah <aaaltameemi@madisoncollege.edu>
2023-07-09 14:18:23 -0500

    Apache_log_milston4

--- Apache_log_Analyzer.py
+++ Apache_log_Analyzer.py
@@ -12,25 +12,28 @@ print(welcome_message)
 #user_message=input("would like to continue?please enter your 'y' for yes amd 'n for No: ")
 #print(user_message)
 #create apache log with multiple lines
-apache_log="""111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)" 
-111.222.333.124 HOME - [01/Feb/1998:01:08:46 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 28083 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)" 
-111.222.333.125 AWAY - [01/Feb/1998:01:08:53 -0800] "GET /bannerad/ad7.gif HTTP/1.0" 401 9332 "http://www.referrer.com/bannerad/ba_ad.htm" "Mozilla/4.01 (Macintosh; I; PPC)" 
-111.222.333.126 AWAY - [01/Feb/1998:01:09:14 -0800] "GET /bannerad/click.htm HTTP/1.0" 501 207 "http://www.referrer.com/bannerad/menu.htm" "Mozilla/4.01 (Macintosh; I; PPC)" """
+with open ('m4-access.log','r') as File:
+    apache_log_entries= File.readlines()
 
-#print(apache_log)
-#spliting the log to single log
-apache_log_entries=apache_log.split('\n')
-#print(apache_log_entries)
+    #print(apache_log)
+#create  writable File
+writable_file = open("apache_analysis","w")
 for entry in apache_log_entries:
+    apache_log_field =entry.split(' ')
+    #create return code
+    return_code = apache_log_field[8]
     #print(entry)
-    #create ip address for the apache log
-    ip_address=entry[0:15:1]
-    print(f"log request from:{ip_address:*^22s}")
-    #Split the apache log to the items
-    apache_log_field=apache_log.split(' ')
-   #create return code 
-    return_code=apache_log_field[8]
-    print(f'Return code:{return_code}')
+    #create ip address for apache_log_entries
+    ip_address = entry[0:15:1]
+    if int(return_code) >= 500:
+        writable_file.writelines(f"{ip_address}{return_code}")
+    elif int(return_code) >=400:
+        print(f"{ip_address} -{return_code}")
 
+
+
+    
+    
+    
 
 
