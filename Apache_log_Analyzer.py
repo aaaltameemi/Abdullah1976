fe3c56dd8b1342f57a1d7dd1d98bf1d66d35f270
Abdullah <aaaltameemi@madisoncollege.edu>
2023-07-12 14:41:02 -0500

    milstone 4 correction

--- Apache_log_Analyzer.py
+++ Apache_log_Analyzer.py
@@ -12,23 +12,43 @@ print(welcome_message)
 #user_message=input("would like to continue?please enter your 'y' for yes amd 'n for No: ")
 #print(user_message)
 #create apache log with multiple lines
-with open ('m4-access.log','r') as File:
-    apache_log_entries= File.readlines()
+#apache_log=
+# opning the log file that want to process
+apache_log_file=open('m4-access.log' , 'r')
+#reading the log file 
+apache_log= apache_log_file.read()
 
-    #print(apache_log)
-#create  writable File
-writable_file = open("apache_analysis","w")
+#print(apache_log)
+#open text file 
+apache_log_analysis = open('apache_analysis.txt', 'w')
+#spliting entry log 
+apache_log_entries = apache_log.split( '\n')
+#print(apache_log_entries)
+
+#looping through each entry in the list of log apach_log_entries
 for entry in apache_log_entries:
-    apache_log_field =entry.split(' ')
-    #create return code
-    return_code = apache_log_field[8]
-    #print(entry)
-    #create ip address for apache_log_entries
-    ip_address = entry[0:15:1]
+   # print(entry)
+    #grapping the first 16 character og the log entry whhich is IP address
+    #ip_address = entry[0:15:1]
     
-    writable_file.writelines(f"{ip_address}{return_code}")
+    #split the string to get the return code
+    apache_log_entry_items = entry.split(' ')
+    #extract the IP address
+    ip_address = apache_log_entry_items[0]
+    return_code = apache_log_entry_items[8]
+    summary=(f"{ip_address} - {return_code}")
+    print(summary)
+    #write to the txt file 
+    apache_log_analysis.write(summary + "\n")
+
+#close the text file to prevent overwritten
+apache_log_analysis.closed()  
+
+
     
-    print(f"{ip_address} -{return_code}")
+    
+
+                    
 
 
 
